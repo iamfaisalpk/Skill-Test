56240f3f10c9f957a60993ab7cab74b494af1151
iamfaisalpk <iamfaizy123@gmail.com>
2026-01-08 17:38:17 +0530

    fully finised done skil test ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Skill Test",
   description: "Mini E-commerce Skill Test",
+  icons: {
+    icon: "/images/shoeImage.png",
+    apple: "/images/shoeImage.png",
+  },
 };
 
 export default function RootLayout({
@@ -32,29 +36,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#161616] text-white min-h-screen flex flex-col`}
       >
-        {/* This initializes auth state from localStorage on every load */}
         <AuthInitializer />
-
-        {/* SSR Navbar */}
         <Navbar />
-
-        {/* Page Content */}
         <main className="flex-1">{children}</main>
-
-        {/* SSR Footer */}
         <Footer />
 
-        {/* Toast notifications */}
         <ToastContainer
           position="top-right"
           autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
           theme="dark"
         />
       </body>
